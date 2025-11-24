a3bcdad33793aba6b710273cb8af0012b8cefcd9
VIDHI BHATT <83345228+VidhiBhatt01@users.noreply.github.com>
2025-11-24 00:30:43 -0800

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -380,7 +380,7 @@ with tab_predict:
             st.info("Fill the form and click 'Get Recommendation'.")
 
     # Testimonials
-    st.markdown("### <User testimonials> ")
+    st.markdown("### User Testimonials ")
 
     t_col1, t_col2 = st.columns(2)
 
